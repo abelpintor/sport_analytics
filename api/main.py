f1b87040db4d7552e26925c78e40174f5600d46c
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2024-11-13 21:02:51 -0600

    Update API

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Form, HTTPException, Request, Depends, status
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
@@ -10,9 +10,8 @@ from typing import Optional
 import os
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import sessionmaker
-import models  
-from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
+import models  
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret_key_example")
@@ -27,8 +26,6 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 models.Base.metadata.create_all(bind=engine)
-
-jinja2_template = Jinja2Templates(directory="templates")
 
 def get_db():
     db = SessionLocal()
@@ -55,35 +52,12 @@ def create_user(db: Session, username: str, email: str, password: str, age: int)
     db.refresh(db_user)
     return db_user
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=JSONResponse)
 def root(request: Request):
-    return jinja2_template.TemplateResponse("index.html", {"request": request})
-
-@app.get("/users/dashboard", response_class=HTMLResponse)
-def dashboard(request: Request):
-    
-    token = request.cookies.get("access_token")
-    
-    if token is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-    
-  
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        username: str = payload.get("username")
-        user_id: int = payload.get("id")
-        
-        if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-        
-    except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired or is invalid")
-    
- 
-    return jinja2_template.TemplateResponse("dashboard.html", {"request": request})
+    return {"message": 'API funcionando'}
 
 
-@app.post("/users/login")
+@app.post("/login", response_class=JSONResponse)
 def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.username == username).first()
     
@@ -97,16 +71,10 @@ def login(username: str = Form(...), password: str = Form(...), db: Session = De
   
     token = create_token({"username": user.username, "id": user.id})
     
- 
-    response = RedirectResponse("/users/dashboard", status_code=302)
-    response.set_cookie(
-        key="access_token", 
-        value=token, 
-        max_age=TOKEN_EXPIRATION_TIME,  
-        httponly=True, 
-        secure=True,    
-        samesite="Strict"  
-    )
+    
+    response = JSONResponse({
+        "token": token
+    }, status_code=302)
     
     return response
 
@@ -119,14 +87,14 @@ async def register(username: str = Form(...), email: str = Form(...), password: 
     return {"msg": "User created successfully", "user": user.username}
 
 
-# @app.post("/predict")
-# async def predict_match(request: Request, home_team: str = Form(...), away_team: str = Form(...)): 
-#     #prediction = predict(home_team, away_team, season)
-#     return {
-#         "probabilidad_equipo_1": 20,
-#         "nombre_equipo_local":"America",
-#         "nombre_equipo_visitante":"Chivas",
-#         "goles_promedio": 3,
-#         "corners_promedio": 7,
+@app.post("/predict")
+async def predict_match(request: Request, home_team: str = Form(...), away_team: str = Form(...)): 
+    #prediction = predict(home_team, away_team, season)
+    return {
+        "probabilidad_equipo_1": 20,
+        "nombre_equipo_local":"America",
+        "nombre_equipo_visitante":"Chivas",
+        "goles_promedio": 3,
+        "corners_promedio": 7,
 
-#     }
+    }
